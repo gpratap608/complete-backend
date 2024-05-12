ecde1de1e3ec220606f76cb8e64be1d8e5238a1c
gpratap608 <113552039+gpratap608@users.noreply.github.com>
2024-05-13 01:28:49 +0530

    New Changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-import express from "express"
+//require('dotenv').config({path: './env'})
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+import app from "./app.js"
+dotenv.config({
+    path:'./.env'
+})
 
-const app = express()  
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Some Error Happened in ./src/index.js before listening to app");
+    })
+    app.listen(process.env.PORT||8080,()=>{
+        console.log({
+            serverStatus:`🌐  Application is Running`,
+            URL: `🔗 http://localhost:${process.env.PORT}`
+        });
+    })
+})
+.catch((error)=>{
+    console.log("DB connection Failed from Index.js");
+})
+ 
